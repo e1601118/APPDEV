8a017eff4ca6e02fc5ac4af15956bd05aeb53791
Nguyen The Anh <e1601118@edu.vamk.fi>
2017-04-18 10:41:10 +0300

    Update realtime.php

--- realtime.php
+++ realtime.php
@@ -3,8 +3,8 @@
 	<head>
 		<title>Sound detector - Real time</title>
 	</head>
-	<body style="text-align:center;">
-		<h1>Sound detector project in realtime</h1>
+	<body style="text-align:center; background-color:#ccffff">
+		<h1 style="text-color:red">Sound detector project in realtime</h1>
 		<!-- This php code will write all the recorded-data to <input> with type hidden and number id(s) for all -->
 		<br>
 		<br>
@@ -21,7 +21,7 @@
 				[
 					['Volume level', 'dB'],
 					<?php 
-					
+						// 1 line in sound.log has format as a;b;c;d;e;f;g;h
 						$file = file("sound.log");
 						for ($i = max(0, count($file)-10); $i < count($file); $i++) 
 						{	
@@ -40,6 +40,7 @@
 
 				  var options = 
 				  {
+					backgroundColor: '#ccffcc',
 					title: 'REAL TIME SOUND DETECTOR',
 					hAxis: {
 					  title: 'Time (s)',
